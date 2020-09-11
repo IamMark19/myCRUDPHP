cf30925617fe9e1c0b887f054f41691ce6c0c006
IamMark19 <70473807+IamMark19@users.noreply.github.com>
2020-09-11 14:29:11 +0800

    Update model.php

--- classes/model.php
+++ classes/model.php
@@ -1,60 +1,148 @@
 <?php
-require_once 'connect.php';
-class model extends connection{
 
+require_once 'classes/connect.php';
+require_once 'classes/model.php';
 
-public function DisplayRecord(){
+$obj = new model();
+//$obj = new connection();
 
-            $sql="SELECT * from student";
-            $result = $this->conn->query($sql);
-            if($result->num_rows>0){
-                while($row=$result->fetch_assoc()){
-                    $data[]=$row;
-                }
+if(isset($_POST['submit'])){
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $obj->InsertRecord($name,$email);
+}//if isset close
+if(isset($_POST['update'])){
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+     $editid=$_POST['hid'];
+    $obj->UpdateRecord($name,$email,$editid);
+}//if isset close
+if(isset($_GET['deleteid'])){
+    $delid=$_GET['deleteid'];
+   
+    $obj->DeleteRecord($delid);
+}//if isset close
 
-                    return $data;
-            }//if close
-        }//function close
-        
-        public function DisplayRecordById($editid){
-
-            $sql="SELECT * from student where id='$editid' ";
-            $result = $this->conn->query($sql);
-            if($result->num_rows==1){
-                $row=$result->fetch_assoc();
-                return $row;
-            }
-        }
-        public function InsertRecord($name,$email){
-
-            $sql= "INSERT INTO `student`(`name`, `email`) VALUES ('$name','$email')";
-            $result = $this->conn->query($sql);
-            if($result){
-                header('location:index.php?msg=ins');
-            }else{
-                echo"error".$sql."<br>".$this->conn->error;
-                } 
-        }
-
-        public function UpdateRecord($name,$email,$editid){
-        $sql= "UPDATE student SET name='$name',email='$email' WHERE id='$editid'";
-        $result = $this->conn->query($sql);
-        if($result){
-        header('location:index.php?msg=ups');
-       }else{
-        echo"error".$sql."<br>".$this->conn->error;
-            } 
-        }
-
-        public function DeleteRecord($delid){
-            $sql="DELETE from student WHERE id='$delid'";
-            $result = $this->conn->query($sql);
-            if($result){
-            header('location:index.php?msg=del');
-            }else{
-                echo"error".$sql."<br>".$this->conn->error;
-
-            } 
-            }   
-}
 ?>
+
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Basic Crud</title>
+     <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+     <script type="text/javascript" src="bootstrap/js/bootstap.js"></script>
+
+ </head>
+ <body><br>
+<h2 class="text-center text-info">Basic Crud OOP</h2><br>
+<div class="wrapper">
+<div class="container">
+
+    <!--- success alert -->
+    <?php
+    if(isset($_GET['msg']) and $_GET['msg'] == 'ins'){
+        echo '<div class="alert alert-primary role="alert">Insert Data successfully</div>';
+    }
+    if(isset($_GET['msg']) and $_GET['msg'] == 'ups'){
+        echo '<div class="alert alert-primary role="alert">Update Data successfully</div>';
+    }
+
+    if(isset($_GET['msg']) and $_GET['msg'] == 'del'){
+        echo '<div class="alert alert-primary role="alert">Data Deleted successfully</div>';
+    }
+
+    ?> 
+
+        <?php
+            if(isset($_GET['editid'])){
+            $editid=$_GET['editid'];
+            $myrecord=$obj->DisplayRecordById($editid);
+           
+        ?>
+             <form action="index.php" method="post">
+            <div class="form-group">
+                <label>Name</label>
+                <input type="text" name="name" value="<?php echo $myrecord['name'];?>" placeholder="Enter Your Name" class="form-control" required="">
+            </div>
+            <div class="form-group">
+                <label>Email</label>
+                <input type="text" name="email" value="<?php echo $myrecord['email'];?>" placeholder="Enter Your E-mail" class="form-control" required="">
+            </div>
+            <div class="form-group">
+             <input type="hidden" name="hid"  value="<?php echo $myrecord['id'];?>">
+                <input type="submit" name="update" value="Update " class="btn btn-info">
+            </div>
+
+    </form><br>
+
+        <?php
+        }else{
+        ?>
+    <form action="index.php" method="post">
+        <div class="form-group">
+            <label>Name</label>
+            <input type="text" name="name" placeholder="Enter Your Name" class="form-control" required="">
+        </div>
+        <div class="form-group">
+            <label>Email</label>
+            <input type="text" name="email" placeholder="Enter Your E-mail" class="form-control" required="">
+        </div>
+        <div class="form-group">
+         
+            <input type="submit" name="submit" value="submit " class="btn btn-info">
+        </div></form>
+    <?php }//else close ?>
+   
+           
+        
+             <br>
+    <h4 class="text-center text-danger">Display Records</h4>
+  
+    <form action="index.php" method="get">
+         <input type="text" name="search" placeholder="search" >
+         <button type="submit" name="search" value="search "  class="btn btn-danger">Search</button>
+         </form> 
+
+         <?php 
+      
+         if(isset($_GET['search'])){
+            $s=$_GET['search'];
+           else{
+                $obj= new model(); }?>
+                
+
+    <table class="table table-bordered"> 
+        <tr class="bg-primary text-center">
+        <th>S.No</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Action</th>
+        </tr>
+        <?php 
+       $data=$obj->DisplayRecordbySearch($s);
+       var_dump($data);
+
+       $sno=1;
+        foreach ($data as $value) { ?>
+           <tr class="text-center">
+               <td><?php echo $sno++; ?></td>
+               <td><?php echo $value['name']; ?></td>
+               <td><?php echo $value['email']; ?></td>
+               <td><a href="index.php?editid=<?php echo $value['id']; ?>" class="btn btn-info">EDIT</a>
+                <a href="index.php?deleteid=<?php echo $value['id']; ?>" class="btn btn-danger">DELETE</a>
+                </td>
+            </tr><?php }//foreach close?>
+   </table>
+<?php } 
+
+ ?>
+
+
+
+  
+</div>
+</div>
+ </body> 
+ </html>
